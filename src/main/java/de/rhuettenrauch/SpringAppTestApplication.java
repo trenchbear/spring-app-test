021c589b65d914c11db5f26289dd1c7c389bf965
Roland Hüttenrauch <rhuettenrauch@mytoys.de>
2016-12-06 22:54:10 +0100

    initial commit

---
+++ src/main/java/de/rhuettenrauch/SpringAppTestApplication.java
@@ -0,0 +1,12 @@
+package de.rhuettenrauch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAppTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAppTestApplication.class, args);
+	}
+}
